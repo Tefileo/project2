e1aeca4b1830546d5ae63961a2848a51214e00de
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-05 09:23:38 +0000

    New Commit

--- project2.py
+++ project2.py
@@ -2,7 +2,7 @@ import random
 
 try_again = 'Yes'
 while try_again == 'Yes':
-    best_guess = int(input('is the number im thinking of 1 or 2: '))
+    best_guess = int(input('Is the number im thinking of 1 or 2: '))
     random_answer = random.randrange(1,3)
     if best_guess == random_answer:
         print('You WIN!!')
@@ -10,3 +10,5 @@ while try_again == 'Yes':
     else:
         print('You Lose!, the answer was {}'.format(random_answer))
         try_again =  input('Would you like to try again? (Yes or No) :')
+
+# Comment
